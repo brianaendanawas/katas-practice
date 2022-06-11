ded57f4e46181aab3772e7a9a874cfd6913dc038
Briana Endanawas <brianamay48@gmail.com>
2022-06-11 17:15:55 -0400

    reverse strings

--- reverse.js
+++ reverse.js
@@ -1,1 +1,9 @@
-console.log(process.argv);
+const strings = process.argv.slice(2);
+
+for (let i = 0; i < strings.length; i++) {
+  let string = "";
+  for (let j = strings[i].length - 1; j >= 0; j--) {
+    string += strings[i][j];
+  }
+  console.log(string);
+}
